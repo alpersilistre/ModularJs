c287c46ffe439895478db1005d55713bd27afd6f
Alper <alpersilistre@gmail.com>
2016-01-18 18:52:52 +0200

    Time 14.40 - Part2
    Code updated after this.addPerson ... part.

--- modularJS.js
+++ modularJS.js
@@ -1,43 +1,84 @@
-// var people = [];
-// var template = $('#people-template').html();
-// 
-// $('#peopleModule').find('button').on('click', function(){
-//   
-//   people.push($('#peopleModule').find('input').val());
-//   
-//   // data for mustache template
-//   var data = {
-//     people: people,
-//   };
-//   
-//   $('#peopleModule').find('ul').html(Mustache.render(template, data));
-// 
-// });
-// 
-// $('#peopleModule').find('ul').delegate('i.del', 'click', function(e){
-//   
-//   var $remove = $(e.target).closest('li');
-//   var i = $('#peopleModule').find('ul').find('li').index($remove);
-//   
-//   $remove.remove();
-//   
-//   people.splice(i, 1);
-//   
-// });
+(function() {
+  
+  var people = {
+    people: ['Will', 'John'],
+    init: function init(){
+      this.cacheDom();
+      this.bindEvents();
+      this.render();
+    },
+    cacheDom: function cacheDom(){
+      this.$el = $('#peopleModule');
+      this.$button = this.$el.find('button');
+      this.$input = this.$el.find('input');
+      this.$ul = this.$el.find('ul');
+      this.template = this.$el.find('#people-template').html();
+    },
+    bindEvents: function bindEvents(){
+      this.$button.on('click', this.addPerson.bind(this));
+    },
+    render: function render(){
+      var data = {
+        people: this.people
+      };
+      this.$ul.html(Mustache.render(this.template, data));
+    },
+    addPerson: function addPerson(){
+      this.people.push(this.input.val());
+    }
+    
+  };
 
-// object literal
-var myModule = {
-  name: 'Will',
-  age: 34,
-  sayName: function(){
-    alert(this.name);
-  },
-  setName: function(newName){
-    this.name = newName;
-  }
-};
+  people.init();
 
-myModule.setName('Alper');
-myModule.sayName();
+})();
 
-// I can hack this with like --> myModule.name = 'Something';
+// 
+//   
+//   var people = [];
+//   var template = $('#people-template').html();
+//   
+//   $('#peopleModule').find('button').on('click', function () {
+// 
+//     people.push($('#peopleModule').find('input').val());
+//   
+//     // data for mustache template
+//     var data = {
+//       people: people,
+//     };
+// 
+//     $('#peopleModule').find('ul').html(Mustache.render(template, data));
+// 
+//   });
+// 
+//   $('#peopleModule').find('ul').delegate('i.del', 'click', function (e) {
+// 
+//     var $remove = $(e.target).closest('li');
+//     var i = $('#peopleModule').find('ul').find('li').index($remove);
+// 
+//     $remove.remove();
+// 
+//     people.splice(i, 1);
+// 
+//   });
+//   
+// 
+
+
+
+// // object literal
+// var myModule = {
+//   name: 'Will',
+//   age: 34,
+//   sayName: function(){
+//     alert(this.name);
+//   },
+//   setName: function(newName){
+//     this.name = newName;
+//   }
+// };
+// 
+// myModule.setName('Alper');
+// myModule.sayName();
+// 
+// // I can hack this with like --> myModule.name = 'Something';
